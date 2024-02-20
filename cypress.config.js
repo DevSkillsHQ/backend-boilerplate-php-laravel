02e047db8cdcf2766e82c119722f3c10f682f885
Ehsan Mirzarazi <ehsanmirzarazi@ehsans-mbp.home>
2024-02-20 14:41:47 +0100

    fix: cypress config updated

--- cypress.config.js
+++ cypress.config.js
@@ -6,10 +6,6 @@ module.exports = defineConfig({
   },
   e2e: {
     supportFile: false,
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
+    setupNodeEvents(on, config) {},
   },
 })
